0ba3de239d979dcefe7f1357d00c047ebeda3fac
WilliamBZA <william.brander@gmail.com>
2018-03-13 13:50:02 +0200

    Add render definition

--- SearchWeb/wwwroot/dist/searchmodule.d.ts
+++ SearchWeb/wwwroot/dist/searchmodule.d.ts
@@ -1,3 +1,8 @@
-﻿declare module 'microfrontends.search' {
-    export class SearchInput { }
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+declare module 'microfrontends.search' {
+    export class SearchInput extends React.Component<RouteComponentProps<{}>, {}> {
+        public render(): JSX.Element;
+    }
 }
